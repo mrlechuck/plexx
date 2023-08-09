4d6e66bed1a6a79087941c1cf98488b7065859ac
mrlechuck <robilinux@gmail.com>
2023-08-10 01:44:52 +0200

    update env add sperimental wip script

---
+++ shell/mega_movie_match_test.sh
@@ -0,0 +1,18 @@
+#SPERIMENTAL WIP
+
+source ../.env
+
+term="ritorno al futuro parte"
+
+#URL Encoding Term
+term_encoded=$(printf %s "${term}" | jq -sRr @uri)
+
+response="$(curl \
+  --location 'http://localhost:7878/api/v3/movie/lookup?term='"${term_encoded}" \
+  --header 'X-Api-Key: '"${RADARR_API_KEY}")"
+
+#"$(echo ${response} | jq '. | length')"
+title="$(echo "${response}" | jq -r '.[0].title')"
+year="($(echo "${response}" | jq -r '.[0].year'))"
+
+echo "${title} ${year}"
